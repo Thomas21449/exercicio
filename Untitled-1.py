1071b6d90b606df1c4c416e6f6bdb2bbdb6947c6
Thomas21449 <dcmachado009@gmail.com>
2025-04-16 20:49:42 -0300

    Delete Untitled-1.py

--- Untitled-1.py
+++
@@ -1,9 +0,0 @@
-while True:
-    try:
-      produto = float(input("informe o valor do produto:"))
-      pago = float(input("informe o valor pago:"))
-      troco = pago-produto
-      print(f'O troco é de R$ {troco:.2f}')
-      break
-    except ValueError:
-     print("deve ser um valor floate!!")
